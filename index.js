af92df54064bcff92ead34cc01a4ba6af93e3321
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2024-03-18 18:31:00 +0530

    optimize the code of zone data

--- index.js
+++ index.js
@@ -3,28 +3,25 @@ import { insertData } from './dataHandeler.js';
 
 async function main() {
     const uf1 = 1, uf2 = 2;
+    const url_zones_ZGB = "https://erp.jciindia.in/lib/api/wm/zone/options";
+    const url_zones_NC = "https://erp.jciindia.in/lib/api/app/committee/options";
     try {
-        const zoneData1 = featchzones({ 'additional': { 'UserInterface': uf1 } });
-        // const data = await fetchDataNGB();
-        console.log("------------------------------------------------");
-        zoneData1.then(data => {
-            // Use map to transform the array of objects into the desired format
-            const formattedData = data.map(obj => [obj.value, obj.text, uf1]);
-            console.log(formattedData);
-            insertData(formattedData);
-        }).catch(error => {
-            console.error(error);
-        });
 
-        const zoneData2 = featchzones({ 'additional': { 'UserInterface': uf2 } });
-        zoneData2.then(data => {
-            // Use map to transform the array of objects into the desired format
-            const formattedData = data.map(obj => [obj.value, obj.text, uf2]);
-            console.log(formattedData);
-            insertData(formattedData);
-        }).catch(error => {
-            console.error(error);
-        });
+        const zoneData1 = await featchzones(url_zones_NC,{ 'additional': { 'UserInterface': uf1 } }); // Wait for zoneData1 to be fetched
+        const formattedData1 = zoneData1.map(obj => {
+            if (Number.isInteger(obj.value)) {
+                return [obj.value, obj.text, uf1];
+            } return null;
+        }).filter(data => data !== null); 
+        await insertData(formattedData1); // Wait for the insertion of zoneData1
+
+        const zoneData2 = await featchzones(url_zones_ZGB,{ 'additional': { 'UserInterface': uf2 } }); // Wait for zoneData2 to be fetched
+        const formattedData2 = zoneData2.map(obj => {
+            if (Number.isInteger(obj.value)) {
+                return [obj.value, obj.text, uf2];
+            } return null;
+        }).filter(data => data !== null); 
+        await insertData(formattedData2); // Wait for the insertion of zoneData2
 
         // const membersDetails = featchMemberDetails({'UserInterface': '2','ZoneCode':'2'});
         // console.log("------------------------------------------------");
