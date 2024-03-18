044d92513f40ae97c4bc904be8d912237543bd69
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2024-03-18 18:02:36 +0530

    Added logic to featch member data and added logic to insert option list data.

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-import { fetchDataNGB,featchzones } from './service.js';
-import { insertData } from './dataHandeler.js'; 
+import { fetchDataNGB, featchzones, featchMemberDetails } from './service.js';
+import { insertData } from './dataHandeler.js';
 
 async function main() {
+    const uf1 = 1, uf2 = 2;
     try {
-        const zoneData = featchzones();
-        const data = await fetchDataNGB();
+        const zoneData1 = featchzones({ 'additional': { 'UserInterface': uf1 } });
+        // const data = await fetchDataNGB();
         console.log("------------------------------------------------");
-        zoneData.then(data => {
+        zoneData1.then(data => {
             // Use map to transform the array of objects into the desired format
-            console.log (data);
-            let formattedData = data.map(obj => `[${obj.value},'${obj.text}']`).join(',');
-            formattedData = '[' + formattedData ;
-            formattedData = formattedData + ']' ;
+            const formattedData = data.map(obj => [obj.value, obj.text, uf1]);
+            console.log(formattedData);
             insertData(formattedData);
-        })
-        .catch(error => {
+        }).catch(error => {
             console.error(error);
         });
+
+        const zoneData2 = featchzones({ 'additional': { 'UserInterface': uf2 } });
+        zoneData2.then(data => {
+            // Use map to transform the array of objects into the desired format
+            const formattedData = data.map(obj => [obj.value, obj.text, uf2]);
+            console.log(formattedData);
+            insertData(formattedData);
+        }).catch(error => {
+            console.error(error);
+        });
+
+        // const membersDetails = featchMemberDetails({'UserInterface': '2','ZoneCode':'2'});
+        // console.log("------------------------------------------------");
+        // membersDetails.then(data => {
+        //     // console.log(data);
+
+        // }).catch(error => {
+        //     console.error(error);
+        // })
     } catch (error) {
         console.error('Error in main function:', error);
     }
