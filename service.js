fcdd0e98b91a12bc40bf6525686339fede85e27c
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2024-03-19 02:47:01 +0530

    optimize code

--- service.js
+++ service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-async function featchzones(url_zones, payload) {
+async function featchData(url_zones, payload) {
   try {
     const response = await axios.post(url_zones, payload);
     const records = response.data.records;
@@ -11,14 +11,4 @@ async function featchzones(url_zones, payload) {
   }
 }
 
-async function featchMemberDetails(url_memberlist, payload) {
-  try {
-    const response = await axios.post(url_memberlist, payload)
-    const records = response.data.records;
-    return records;
-  } catch (error) {
-    console.error('Error fetching data from API:', error);
-    throw error;
-  }
-}
-export { featchzones, featchMemberDetails };
+export { featchData };
