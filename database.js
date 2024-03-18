2a30546a1f6481dc14aa09fce47e5f4b6c212424
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2024-03-19 03:59:44 +0530

    added create table query

--- database.js
+++ database.js
@@ -3,11 +3,27 @@ import mysql from 'mysql2';
 // Create a pool to handle multiple connections
 const pool = mysql.createPool({
     host:'127.0.0.1',
-    user:'root',
-    password: 'toor',
-    database: 'jci_db'
+    user:'****',          //Replace it with your username   
+    password: '******',   //Replace it with your password 
+    database: 'jci_db'    //Replace it with your db 
 }).promise();
 
 // Export the pool
 // module.exports = pool;
 export default pool;
+
+
+
+/*
+Create Table in db 
+
+create table memberDetails(
+member_Id int,
+member_name varchar(225),
+zone_id int,
+zone_name varchar(225),
+emailid varchar(225),
+mobile_no varchar(13)
+);
+
+*/
