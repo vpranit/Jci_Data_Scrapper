ab5506e9b8463a0c7d1f147f062bfbf85edee966
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2024-03-19 03:59:14 +0530

    cleaned code

--- dataHandeler.js
+++ dataHandeler.js
@@ -14,8 +14,6 @@ async function insertZoneData(values){
 async function insertMemberData(values){
     try {
         const sql = 'INSERT INTO memberdetails (member_Id, member_name, zone_id, zone_name, emailid, mobile_no) VALUES ?';
-        console.log(values);
-        // Execute the query using the promise-based pool
         const result = await pool.query(sql, [values.map(row => [row[0], row[1], row[2], row[3], row[4], row[5]])]);
         console.log('Data inserted successfully:', result);
     } catch (error) {
